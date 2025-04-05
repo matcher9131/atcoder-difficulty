d8a757a8d21a1f9b14dade1731fdfc2ef2933bea
matcher9131 <matcher9131cd@gmail.com>
2025-04-05 12:09:06 +0900

    Implement search_difficulty (use ternary search)

--- main.py
+++ main.py
@@ -20,37 +20,48 @@ with open(filepath, "r", encoding="utf-8") as json_file:
 # }
 
 problems = list(map(lambda element: element["TaskScreenName"], json_data["TaskInfo"]))
-abilities = np.array([player["OldRating"] for player in json_data["StandingsData"]])
-responses = np.zeros((len(abilities), len(problems)))
-for player_index, player in enumerate(json_data["StandingsData"]):
-    for problem_index, problem in enumerate(problems):
-        if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] > 0:
-            responses[player_index][problem_index] = 1
+players = [player for player in json_data["StandingsData"]]
+abilities = [player["OldRating"] for player in players]
+responses = [
+    [1 if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] else 0 for player in players] for problem in problems
+]
 
-discrimination = 1.5
+discrimination = 1.7
 
 def two_pl_model(ability, discrimination, difficulty):
     return 1.0 / (1.0 + np.exp(-discrimination * (ability - difficulty) / 400))
 
-def search_difficulty(abilities, responses, discrimination, max_iterations = 1000):
+def sum_likelihood(abilities, responses, discrimination, difficulty):
+    sum = 0.0
+    for player_index, ability in enumerate(abilities):
+        response = responses[player_index]
+        probability = two_pl_model(ability, discrimination, difficulty)
+        sum += np.log(probability) if response else np.log(1 - probability)
+    return sum
+
+def search_difficulty(abilities, responses, discrimination, epsilon = 1e-8):
     difficulties = []
-    for problem_index in range(len(responses[0])):
-        left = -1000
-        right = 5000
-        while right - left > 1:
-            mid = (left + right) // 2
-            sum_likelihood = 0.0
-            for player_index, ability in enumerate(abilities):
-                response = responses[player_index][problem_index]
-                probability = two_pl_model(ability, discrimination, mid)
-                sum_likelihood += response * np.log(probability) + (1 - response) * np.log(1 - probability)
-            # sum_likelihoodと0の比較ではなくsum_likelihoodの極大値を探したい
-            # 三分探索ならいけるかも？
-            if sum_likelihood > 0.0:
-                right = mid
+    for problem_index in range(len(problems)):
+        #
+        print("problem_index = ", problem_index)
+        #
+        likelihood_memo = {}
+        left = -1000.0
+        right = 5000.0
+        while True:
+            c1 = (2 * left + right) / 3
+            c2 = (left + 2 * right) / 3
+            fc1 = likelihood_memo[c1] if c1 in likelihood_memo else sum_likelihood(abilities, responses[problem_index], discrimination, c1)
+            likelihood_memo[c1] = fc1
+            fc2 = likelihood_memo[c2] if c2 in likelihood_memo else sum_likelihood(abilities, responses[problem_index], discrimination, c2)
+            likelihood_memo[c2] = fc2
+            if fc1 < fc2:
+                left = c1
             else:
-                left = mid
-        difficulties.append(mid)
+                right = c2
+            if (abs(fc1 - fc2) < epsilon):
+                break
+        difficulties.append((left + right) / 2)
     return difficulties
 
 difficulties = search_difficulty(abilities, responses, discrimination)
