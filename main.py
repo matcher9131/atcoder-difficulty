52e50cd1894df998e99f6283e77d439086374690
matcher9131 <matcher9131cd@gmail.com>
2025-04-06 11:13:48 +0900

    Search difficulites of multiple contests

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
+import os
+import glob
 import json
 import numpy as np
-from scipy.optimize import minimize
 
-
-contest_name = "abc396"
-
-filepath = f"data/{contest_name}.json"
-with open(filepath, "r", encoding="utf-8") as json_file:
-    json_data = json.load(json_file)
-
-# type JsonData = {
-#     TaskInfo: Array<{ TaskScreenName: `${ContestName}_${ProblemIndex}`}>
-#     StandingsData: Array<
-#         OldRating: integer
-#         TaskResults: {
-#             `${ContestName}_${ProblemIndex}`: {
-#                 SubmissionID: integer    // more than 0 for AC submission, 0 for otherwise
-#             }
-#         }
-#     >
-# }
-
-problems = list(map(lambda element: element["TaskScreenName"], json_data["TaskInfo"]))
-players = [player for player in json_data["StandingsData"] if player["OldRating"] != 0]
-abilities = [player["OldRating"] for player in players]
-responses = [
-    [1 if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] > 0 else 0 for player in players] for problem in problems
-]
+contest_name = "abc400"
 
 def two_pl_model(ability, discrimination, difficulty):
-    return 1.0 / (1.0 + np.exp(-discrimination * (ability - difficulty) / 400.0))
+    return 1.0 / (1.0 + np.exp(-discrimination * (ability - difficulty)))
 
-def sum_likelihood(abilities, responses, discrimination, difficulty):
-    sum = 0.0
-    for player_index, ability in enumerate(abilities):
-        response = responses[player_index]
-        probability = two_pl_model(ability, discrimination, difficulty)
-        sum += np.log(probability) if response else np.log(1 - probability)
-    return sum
-
-def search_difficulty1(abilities, responses):
-    discrimination = np.log(6)
+def search_difficulty(abilities, responses):
+    discrimination = np.log(6) / 400.0
     difficulties = []
-    for problem_index in range(len(problems)):
+    for problem_index in range(len(responses)):
         num_acs = sum(responses[problem_index])
         left = -1000
         right = 5000
@@ -58,63 +27,29 @@ def search_difficulty1(abilities, responses):
         difficulties.append(left)
     return difficulties
 
-def search_difficulty2(abilities, responses, epsilon = 1e-8):
-    discrimination = np.log(6)
-    difficulties = []
-    for problem_index in range(len(problems)):
-        likelihood_memo = {}
-        left = -1000
-        right = 5000
-        while True:
-            c1 = (2 * left + right) / 3
-            c2 = (left + 2 * right) / 3
-            fc1 = likelihood_memo[c1] if c1 in likelihood_memo else sum_likelihood(abilities, responses[problem_index], discrimination, c1)
-            likelihood_memo[c1] = fc1
-            fc2 = likelihood_memo[c2] if c2 in likelihood_memo else sum_likelihood(abilities, responses[problem_index], discrimination, c2)
-            likelihood_memo[c2] = fc2
-            if fc1 < fc2:
-                left = c1
-            else:
-                right = c2
-            if (abs(fc1 - fc2) < epsilon):
-                break
-        difficulties.append(round((left + right) / 2))
-    return difficulties
+def get_difficulty(contest_json_data):
+    problems = [element["TaskScreenName"] for element in contest_json_data["TaskInfo"]]
+    players = [player for player in contest_json_data["StandingsData"] if player["OldRating"] != 0]
+    abilities = [player["OldRating"] for player in players]
+    responses = [
+        [1 if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] > 0 else 0 for player in players] for problem in problems
+    ]
+    return search_difficulty(abilities, responses)
 
 
+output_filepath = "output/difficulties.json"
+with open(output_filepath, "r", encoding="utf-8") as output_file:
+    difficulties_json_data = json.load(output_file)
 
-def search_difficulty3(abilities, responses):
-    def two_pl_model2(scaled_ability, discrimination, scaled_difficulty):
-        return 1.0 / (1.0 + np.exp(-discrimination * (scaled_ability - scaled_difficulty)))
-    def likelihood(params, responses, abilities):
-        discrimination, scaled_difficulty = params
-        sum_likelihood = 0
-        for response, scaled_ability in zip(responses, abilities):
-            probability = two_pl_model2(scaled_ability, discrimination, scaled_difficulty)
-            sum_likelihood += np.log(probability) if response else np.log(1 - probability)
-        return -sum_likelihood
+files = glob.glob("./data/*.json")
+for filepath in files:
+    contest_name = os.path.splitext(os.path.basename(filepath))[0]
+    if not contest_name in difficulties_json_data:
+        with open(filepath, "r", encoding="utf-8") as json_file:
+            json_data = json.load(json_file)
+        difficulties = get_difficulty(json_data)
+        difficulties_json_data[contest_name] = difficulties
 
-    _abilities = np.array(abilities)
-    mean = np.mean(_abilities)
-    std = np.std(_abilities)
-    scaled_abilities = (_abilities - mean) / std
-    difficulties = []
-    for problem_index in range(len(problems)):
-        result = minimize(
-            likelihood,
-            [1.0, 0.0],
-            args=(responses[problem_index], scaled_abilities),
-            method="L-BFGS-B"
-        )
-
-        if result.success:
-            _, scaled_difficulty = result.x
-            difficulties.append(int(scaled_difficulty * std + mean))
-        else:
-            difficulties.append("ERROR")
-            print(result.message)
-    return difficulties
-
-print(search_difficulty1(abilities, responses))
-# print(search_difficulty2(abilities, responses))
-# print(search_difficulty3(abilities, responses))
+with open(output_filepath, "w", encoding="utf-8") as output_file:
+    output_file.write(json.dumps(difficulties_json_data, indent=4))
+            
