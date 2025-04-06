8175c817a3427bf21da40be22de6921e88dfcd7f
matcher9131 <matcher9131cd@gmail.com>
2025-04-06 10:44:10 +0900

    Implement another method of search_difficulty

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import numpy as np
 import json
+import numpy as np
+from scipy.optimize import minimize
+
 
 contest_name = "abc396"
 
-filepath = "data/" + contest_name + ".json"
+filepath = f"data/{contest_name}.json"
 with open(filepath, "r", encoding="utf-8") as json_file:
     json_data = json.load(json_file)
 
@@ -20,16 +22,14 @@ with open(filepath, "r", encoding="utf-8") as json_file:
 # }
 
 problems = list(map(lambda element: element["TaskScreenName"], json_data["TaskInfo"]))
-players = [player for player in json_data["StandingsData"]]
+players = [player for player in json_data["StandingsData"] if player["OldRating"] != 0]
 abilities = [player["OldRating"] for player in players]
 responses = [
-    [1 if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] else 0 for player in players] for problem in problems
+    [1 if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] > 0 else 0 for player in players] for problem in problems
 ]
 
-discrimination = 1.7
-
 def two_pl_model(ability, discrimination, difficulty):
-    return 1.0 / (1.0 + np.exp(-discrimination * (ability - difficulty) / 400))
+    return 1.0 / (1.0 + np.exp(-discrimination * (ability - difficulty) / 400.0))
 
 def sum_likelihood(abilities, responses, discrimination, difficulty):
     sum = 0.0
@@ -39,15 +39,32 @@ def sum_likelihood(abilities, responses, discrimination, difficulty):
         sum += np.log(probability) if response else np.log(1 - probability)
     return sum
 
-def search_difficulty(abilities, responses, discrimination, epsilon = 1e-8):
+def search_difficulty1(abilities, responses):
+    discrimination = np.log(6)
     difficulties = []
     for problem_index in range(len(problems)):
-        #
-        print("problem_index = ", problem_index)
-        #
+        num_acs = sum(responses[problem_index])
+        left = -1000
+        right = 5000
+        while right - left > 1:
+            mid = (left + right) // 2
+            expected_num_acs = 0
+            for ability in abilities:
+                expected_num_acs += two_pl_model(ability, discrimination, mid)
+            if expected_num_acs < num_acs:
+                right = mid
+            else:
+                left = mid
+        difficulties.append(left)
+    return difficulties
+
+def search_difficulty2(abilities, responses, epsilon = 1e-8):
+    discrimination = np.log(6)
+    difficulties = []
+    for problem_index in range(len(problems)):
         likelihood_memo = {}
-        left = -1000.0
-        right = 5000.0
+        left = -1000
+        right = 5000
         while True:
             c1 = (2 * left + right) / 3
             c2 = (left + 2 * right) / 3
@@ -61,8 +78,43 @@ def search_difficulty(abilities, responses, discrimination, epsilon = 1e-8):
                 right = c2
             if (abs(fc1 - fc2) < epsilon):
                 break
-        difficulties.append((left + right) / 2)
+        difficulties.append(round((left + right) / 2))
     return difficulties
 
-difficulties = search_difficulty(abilities, responses, discrimination)
-print(difficulties)
+
+
+def search_difficulty3(abilities, responses):
+    def two_pl_model2(scaled_ability, discrimination, scaled_difficulty):
+        return 1.0 / (1.0 + np.exp(-discrimination * (scaled_ability - scaled_difficulty)))
+    def likelihood(params, responses, abilities):
+        discrimination, scaled_difficulty = params
+        sum_likelihood = 0
+        for response, scaled_ability in zip(responses, abilities):
+            probability = two_pl_model2(scaled_ability, discrimination, scaled_difficulty)
+            sum_likelihood += np.log(probability) if response else np.log(1 - probability)
+        return -sum_likelihood
+
+    _abilities = np.array(abilities)
+    mean = np.mean(_abilities)
+    std = np.std(_abilities)
+    scaled_abilities = (_abilities - mean) / std
+    difficulties = []
+    for problem_index in range(len(problems)):
+        result = minimize(
+            likelihood,
+            [1.0, 0.0],
+            args=(responses[problem_index], scaled_abilities),
+            method="L-BFGS-B"
+        )
+
+        if result.success:
+            _, scaled_difficulty = result.x
+            difficulties.append(int(scaled_difficulty * std + mean))
+        else:
+            difficulties.append("ERROR")
+            print(result.message)
+    return difficulties
+
+print(search_difficulty1(abilities, responses))
+# print(search_difficulty2(abilities, responses))
+# print(search_difficulty3(abilities, responses))
