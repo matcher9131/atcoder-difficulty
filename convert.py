20bc53df540cb46a0a792415754e0601c2b99cb5
matcher9131 <matcher9131cd@gmail.com>
2025-04-08 09:24:33 +0900

    Fix convert.py (Exclude contestants with no submissions and Use 'Raw' ratings)

--- convert.py
+++ convert.py
@@ -1,11 +1,19 @@
 import os
 import glob
 from json_io import load_json, save_json
+from numpy import sqrt, isnan
+
+def get_rating_adjustment(num_competitions: int) -> float:
+    r = (sqrt(1.0 - (0.9 ** (2 * num_competitions))) / (1.0 - 0.9 ** num_competitions) - 1.0) / (sqrt(19) - 1.0) * 1200.0
+    if (isnan(r)):
+        print(f"Invalid num_competitions = {num_competitions}")
+        return 0
+    return r
 
 def contest_json_to_data(contest_json) -> tuple[list[float], list[list[float]]]:
     problems = [element["TaskScreenName"] for element in contest_json["TaskInfo"]]
-    players = [player for player in contest_json["StandingsData"] if player["OldRating"] != 0]
-    abilities = [player["OldRating"] for player in players]
+    players = [player for player in contest_json["StandingsData"] if player["Competitions"] > 0 and player["OldRating"] > 0 and player["TotalResult"]["Count"] > 0]
+    abilities = [player["OldRating"] + get_rating_adjustment(player["Competitions"]) for player in players]
     responses = [
         [1 if problem in player["TaskResults"] and player["TaskResults"][problem]["SubmissionID"] > 0 else 0 for player in players] for problem in problems
     ]
