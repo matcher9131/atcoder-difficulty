f4cf466d5c3838c9c8a9a5462dffac27f3b9a6f1
matcher9131 <matcher9131cd@gmail.com>
2025-04-26 08:27:17 +0900

    Add tests of estimate_difficulties.get_num_contests

--- tests/operations/test_estimate_difficulties.py
+++ tests/operations/test_estimate_difficulties.py
@@ -1,8 +1,9 @@
 
 import pytest
 
+from models.contest_entry import ContestEntry
 from models.contest_info import ContestsNeedingHistory
-from operations.estimate_difficulties import contest_needs_history
+from operations.estimate_difficulties import contest_needs_history, get_num_contests
 
 
 @pytest.mark.parametrize(("contest_name", "expected"), [
@@ -18,3 +19,24 @@ from operations.estimate_difficulties import contest_needs_history
 def test_contest_needs_history(contest_name: str, expected: bool):
     contests_needing_history: ContestsNeedingHistory = { "abc": 399, "arc": 196, "agc": 71 }
     assert contest_needs_history(contests_needing_history, contest_name) == expected
+
+@pytest.mark.parametrize(("contest_name", "expected"), [
+    ("abc001", 0),
+    ("abc002", 0),
+    ("abc003", 1),
+    ("abc004", 1),
+    ("abc005", 1),
+    ("abc006", 2),
+    ("abc007", 3),
+])
+def test_get_num_contests(contest_name: str, expected: bool):
+    entries: list[ContestEntry] = [
+        { "contest": "abc001", "isRated": False },
+        { "contest": "abc002", "isRated": True },
+        { "contest": "abc003", "isRated": False },
+        { "contest": "abc004", "isRated": False },
+        { "contest": "abc005", "isRated": True },
+        { "contest": "abc006", "isRated": True },
+        { "contest": "abc007", "isRated": False },
+    ]
+    assert get_num_contests(entries, contest_name) == expected
