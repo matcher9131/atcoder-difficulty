44ddc9efd676800d4b72884782092b1a368817b5
matcher9131 <matcher9131cd@gmail.com>
2025-04-29 12:17:40 +0900

    Add rules to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,13 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from 'eslint-config-prettier'
-
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
     tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
+    { "rules": { "react/react-in-jsx-scope": "off" }},
     eslintConfigPrettier
 ]);
