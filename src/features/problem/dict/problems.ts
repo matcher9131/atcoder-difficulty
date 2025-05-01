5a4f03e41088a873b75fa8ea455cc0e42a7af8d8
matcher9131 <matcher9131cd@gmail.com>
2025-05-01 12:03:43 +0900

    Fix problemsAtom (await loading)

--- src/features/problem/dict/problems.ts
+++ src/features/problem/dict/problems.ts
@@ -2,15 +2,15 @@ import { atom } from "jotai";
 import type { Problems } from "../types/problems";
 import { atomFamily } from "jotai/utils";
 
-const loadProblems = async () => {
+const loadProblems = async (): Promise<Problems> => {
     const response = await fetch("/problems.json");
     if (!response.ok) throw new Error("Failed loading problems.");
     const json = await response.json();
     return json as Problems;
 };
 
-const problemsAtom = atom(loadProblems());
+const problemsAtom = atom(await loadProblems());
 
 export const problemSelector = atomFamily((problemId: string) =>
-    atom(async (get) => (await get(problemsAtom))[problemId]),
+    atom((get) => get(problemsAtom)[problemId]),
 );
