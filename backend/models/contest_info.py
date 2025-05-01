85e844d8ad366f5a0feb6dce739227cfc9344f96
matcher9131 <matcher9131cd@gmail.com>
2025-05-01 11:53:22 +0900

    Fix ContestInfo (Fix type hints)

--- backend/models/contest_info.py
+++ backend/models/contest_info.py
@@ -5,7 +5,7 @@ from util.json_io import load_json
 
 class ContestInfoItem(TypedDict):
     date: str
-    rating: dict[str, int]
+    rating: dict[str, int | None] | None
 
 def load_contest_info() -> dict[str, ContestInfoItem]:
     return load_json("input/contest_info.json")
