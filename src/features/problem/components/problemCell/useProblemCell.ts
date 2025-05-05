8062579550123f62756b2e5426761bc96f76ca2f
matcher9131 <matcher9131cd@gmail.com>
2025-05-05 13:57:17 +0900

    Fix bugs: Wrong SVG with ProblemCell

--- src/features/problem/components/problemCell/useProblemCell.ts
+++ src/features/problem/components/problemCell/useProblemCell.ts
@@ -6,7 +6,7 @@ import type { ProblemCellProps } from "./ProblemCell";
 export const useProblemCell = (problemId: string): ProblemCellProps => {
     const [problem] = useAtom(problemSelector(problemId));
     const [, difficulty] = problem.d ?? [null];
-    const level = difficulty != null ? (difficulty >= 3200 ? 4 : (Math.floor(difficulty / 50) % 4) + 1) : 0;
+    const level = difficulty != null ? (difficulty >= 3200 ? 4 : (Math.floor(difficulty / 100) % 4) + 1) : 0;
     const iconHref = `/resources/up_arrow_${level}.svg#up_arrow_${level}`;
     const fillColor = difficulty != null ? getFillColor(difficulty) : "";
     const textColor = difficulty != null ? getTextColor(difficulty) : "";
