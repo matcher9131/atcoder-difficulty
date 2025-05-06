e07cdaffce30d03e61dbc0e577c0fc35e2be63e2
matcher9131 <matcher9131cd@gmail.com>
2025-05-06 10:18:00 +0900

    Fix useContestTable (Implement pagination)

--- src/features/contest/components/contestsTable/useContestsTable.tsx
+++ src/features/contest/components/contestsTable/useContestsTable.tsx
@@ -4,12 +4,14 @@ import { contestIdsByTypeAtom } from "../../dict/contests";
 import type { ContestsTableProps } from "./ContestsTable";
 import { ContestRowContainer } from "../contestRow";
 import { getNumProblems } from "./funcitons";
+import { paginationValueAtom } from "../../../pagination/model/paginations";
 
 export const useContestsTable = (contestType: ContestType): ContestsTableProps => {
     const numProblems = getNumProblems(contestType);
     const [contestIds] = useAtom(contestIdsByTypeAtom(contestType));
-    const contestRows = contestIds.map((contestId) => (
-        <ContestRowContainer key={contestId} contestId={contestId} numProblems={numProblems} />
-    ));
+    const [pageIndex] = useAtom(paginationValueAtom(contestType));
+    const contestRows = contestIds
+        .slice(100 * pageIndex, 100 * (pageIndex + 1))
+        .map((contestId) => <ContestRowContainer key={contestId} contestId={contestId} numProblems={numProblems} />);
     return { numProblems, contestRows };
 };
