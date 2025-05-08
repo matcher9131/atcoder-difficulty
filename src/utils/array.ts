543203f1a463bbda596ad5d9eb27243b9974342e
matcher9131 <matcher9131cd@gmail.com>
2025-05-08 12:12:16 +0900

    Add binarySearch to util

---
+++ src/utils/array.ts
@@ -0,0 +1,13 @@
+export const binarySearch = <T>(arr: readonly T[], selector: (x: T) => number, value: number): number => {
+    let left = 0;
+    let right = arr.length;
+    while (right - left > 0) {
+        const mid = Math.floor((left + right) / 2);
+        if (selector(arr[mid]) > value) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+};
