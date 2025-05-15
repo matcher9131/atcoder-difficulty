c1e5db2adeba878790ed36be8aca80cccf1cad6c
matcher9131 <matcher9131cd@gmail.com>
2025-05-15 10:56:13 +0900

    Fix useRatingInput (Reset pagination of solve probability tab when inputted rating is changed)

--- src/features/rating/components/ratingInput/useRatingInput.ts
+++ src/features/rating/components/ratingInput/useRatingInput.ts
@@ -3,11 +3,13 @@ import { numContestsAtom, ratingAtom } from "../../models/rating";
 import { useRef, type ChangeEvent } from "react";
 import { parseIntOrNull } from "../../../../utils/number";
 import type { RatingInputProps } from "./RatingInput";
+import { paginationValueAtom } from "../../../pagination/model/paginations";
 
 const delay = 500;
 
 export const useRatingInput = (): RatingInputProps => {
-    const [, setRating] = useAtom(ratingAtom);
+    const [rating, setRating] = useAtom(ratingAtom);
+    const [, setSolveProbabilityPaginationValue] = useAtom(paginationValueAtom("solveProbability"));
     const ratingInputRef = useRef<HTMLInputElement>(null);
     const ratingTimeoutRef = useRef<number | null>(null);
     const handleRatingChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -16,7 +18,10 @@ export const useRatingInput = (): RatingInputProps => {
         }
         const value = e.target.checkValidity() ? parseIntOrNull(e.target.value) : null;
         ratingTimeoutRef.current = setTimeout(() => {
-            setRating(value);
+            if (value !== rating) {
+                setSolveProbabilityPaginationValue(0);
+                setRating(value);
+            }
         }, delay);
     };
 
