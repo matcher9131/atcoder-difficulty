d21418d56c87bfa1338ea92a81cad94db067237d
matcher9131 <matcher9131cd@gmail.com>
2025-05-22 13:02:42 +0900

    Add distributions to models

---
+++ src/features/distribution/models/distributions.ts
@@ -0,0 +1,44 @@
+import { atom } from "jotai";
+import { atomFamily, atomWithDefault } from "jotai/utils";
+import distributionChunkLastIdsUrl from "../../../assets/distribution_chunk.json?url";
+
+const encodedDistributionFiles = import.meta.glob("/assets/distributions/*.json");
+
+const loadEncodedDistributionChunk = async (chunkIndex: number): Promise<Record<string, string>> => {
+    const filename = `distribution${chunkIndex.toString()}.json`;
+    const matchedKey = Object.keys(encodedDistributionFiles).find((key) => key.endsWith(filename));
+    if (matchedKey == null) throw new Error(`${filename} is not found.`);
+    return (await encodedDistributionFiles[matchedKey]()) as Record<string, string>;
+};
+
+const encodedDistributionChunkAtom = atomFamily((chunkIndex: number) =>
+    atomWithDefault(async () => {
+        const encodedChunk = await loadEncodedDistributionChunk(chunkIndex);
+        return new Map(Object.entries(encodedChunk));
+    }),
+);
+
+const loadDistributionChunkLastIds = async (): Promise<readonly string[]> => {
+    const response = await fetch(distributionChunkLastIdsUrl);
+    if (!response.ok) throw new Error("Failed loading distribution chunks.");
+    const json = (await response.json()) as string[];
+    return json;
+};
+
+const distributionChunkLastIds = atom(await loadDistributionChunkLastIds());
+
+export const distributionAtom = atomFamily((problemId: string) =>
+    atom((get) => {
+        let chunkIndex = 0;
+        const lastIds = get(distributionChunkLastIds);
+        for (; chunkIndex < lastIds.length; ++chunkIndex) {
+            if (problemId.replaceAll("/", "~") < lastIds[chunkIndex]) break;
+        }
+        if (chunkIndex >= get(distributionChunkLastIds).length) throw new Error(`Invalid problemId: ${problemId}`);
+        return get(encodedDistributionChunkAtom(chunkIndex)).then((encodedDistributionChunk) => {
+            const encodedDistribution = encodedDistributionChunk.get(problemId);
+            if (encodedDistribution == null) throw new Error(`Invalid problemId: ${problemId}`);
+            return Uint8Array.from(atob(encodedDistribution), (ch) => ch.charCodeAt(0));
+        });
+    }),
+);
