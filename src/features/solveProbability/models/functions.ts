a37e99266c6d3d89170a77dde846a3774bbae471
matcher9131 <matcher9131cd@gmail.com>
2025-05-24 09:21:23 +0900

    Fix irt2pl in frontend (use safe sigmoid)

--- src/features/solveProbability/models/functions.ts
+++ src/features/solveProbability/models/functions.ts
@@ -1,9 +1,13 @@
 import type { ProblemSolveProbability } from "../types/problemSolveProbability";
 
+const safeSigmoid = (x: number): number => {
+    return x >= 0 ? 1 / (1 + Math.exp(-x)) : Math.exp(x) / (1 + Math.exp(x));
+};
+
 const stddev = 600;
 
 export const irt2pl = (ability: number, discrimination: number, difficulty: number) =>
-    1 / (1 + Math.exp((-discrimination * (ability - difficulty)) / stddev));
+    safeSigmoid(discrimination * ((ability - difficulty) / stddev));
 
 export const compareSolveProbability = (x: ProblemSolveProbability, y: ProblemSolveProbability): number => {
     return x.solveProbability - y.solveProbability;
