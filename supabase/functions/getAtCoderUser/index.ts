2f987d1928a55467356f3b659950e628884d2c53
matcher9131 <matcher9131cd@gmail.com>
2025-06-07 10:14:10 +0900

    Fix deno.json (about settings of fmt)

--- supabase/functions/getAtCoderUser/index.ts
+++ supabase/functions/getAtCoderUser/index.ts
@@ -7,34 +7,33 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.49/deno-dom-wasm.ts";
 
 Deno.serve(async (request) => {
-  const { userName } = await request.json();
-  if (userName == null) {
-    return new Response("User name is missing", { status: 400 });
-  }
+    const { userName } = await request.json();
+    if (userName == null) {
+        return new Response("User name is missing", { status: 400 });
+    }
 
-  const externalResponse = await fetch(`https://atcoder.jp/users/${userName}`);
-  if (externalResponse.status === 404) {
-    return new Response("User not found", { status: 404 });
-  } else if (externalResponse.status !== 200) {
-    throw new Error("Failed to fetch");
-  }
+    const externalResponse = await fetch(`https://atcoder.jp/users/${userName}`);
+    if (externalResponse.status === 404) {
+        return new Response("User not found", { status: 404 });
+    } else if (externalResponse.status !== 200) {
+        throw new Error("Failed to fetch");
+    }
 
-  const html = await externalResponse.text();
-  const parser = new DOMParser().parseFromString(html, "text/html");
-  const rating = parseInt(
-    [...parser.querySelectorAll("tr")].find((tr) =>
-      tr.querySelector("th")?.textContent == "Rating"
-    )?.querySelector("td span")?.textContent ?? "",
-  );
-  const numContests = parseInt(
-    [...parser.querySelectorAll("tr")].find((tr) =>
-      tr.querySelector("th")?.textContent == "Rated Matches "
-    )?.querySelector("td")?.textContent ?? "",
-  );
-  const data = { rating, numContests };
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
-  });
+    const html = await externalResponse.text();
+    const parser = new DOMParser().parseFromString(html, "text/html");
+    const rating = parseInt(
+        [...parser.querySelectorAll("tr")].find((tr) => tr.querySelector("th")?.textContent == "Rating")?.querySelector(
+            "td span",
+        )?.textContent ?? "",
+    );
+    const numContests = parseInt(
+        [...parser.querySelectorAll("tr")].find((tr) => tr.querySelector("th")?.textContent == "Rated Matches ")
+            ?.querySelector("td")?.textContent ?? "",
+    );
+    const data = { rating, numContests };
+    return new Response(JSON.stringify(data), {
+        headers: { "Content-Type": "application/json" },
+    });
 });
 
 /* To invoke locally:
