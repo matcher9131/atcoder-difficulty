c0dd00cc0d6ff17db189508fbb3af45432a8d109
matcher9131 <matcher9131cd@gmail.com>
2025-06-10 11:27:57 +0900

    Add apply button to UserInput

--- src/features/user/components/userInput/UserInput.tsx
+++ src/features/user/components/userInput/UserInput.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
-import type { ChangeEventHandler, ReactNode, Ref } from "react";
+import type { ReactNode, Ref } from "react";
 import { useTranslation } from "react-i18next";
 import type { UserNameValidationState } from "../../types/userNameValidationState";
 
 export type UserInputProps = {
     readonly inputRef: Ref<HTMLInputElement>;
-    readonly handleChange: ChangeEventHandler<HTMLInputElement>;
+    readonly buttonIsDisabled: boolean;
+    readonly handleClick: () => void;
     readonly validationState: UserNameValidationState;
     readonly validationMessage: string;
     readonly className?: string;
@@ -13,7 +14,8 @@ export type UserInputProps = {
 
 export const UserInput = ({
     inputRef,
-    handleChange,
+    buttonIsDisabled,
+    handleClick,
     validationState,
     validationMessage,
     className,
@@ -24,15 +26,16 @@ export const UserInput = ({
     return (
         <div className={clsx("text-left", "flex", "gap-x-2", "items-center", className)}>
             <label className={clsx("input", labelStateClass, "w-60")}>
-                <span className="text-sm">{t("userInput.label")}</span>
-                <input
-                    type="text"
-                    ref={inputRef}
-                    pattern="^[0-9A-Za-z_]+$"
-                    onChange={handleChange}
-                    className="grow text-right"
-                />
+                <span className="text-sm">{t("userInput.inputLabel")}</span>
+                <input type="text" ref={inputRef} pattern="^[0-9A-Za-z_]+$" className="grow text-right" />
             </label>
+            <button disabled={buttonIsDisabled} onClick={handleClick} className="btn btn-primary w-16">
+                {buttonIsDisabled ? (
+                    <div className="animate-spin h-4 w-4 border-primary border-1 border-t-transparent rounded-full"></div>
+                ) : (
+                    t("userInput.buttonLabel")
+                )}
+            </button>
             <div className="text-error">{validationMessage}</div>
         </div>
     );
