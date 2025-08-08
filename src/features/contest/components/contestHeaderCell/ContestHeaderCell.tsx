3d51a498ddabccb01d6ba6f1746d799f9e60eaab
matcher9131 <matcher9131cd@gmail.com>
2025-08-08 11:34:36 +0900

    Add ContestStatsDialog to MainPage and a button to show stats to ContestHeaderCell

--- src/features/contest/components/contestHeaderCell/ContestHeaderCell.tsx
+++ src/features/contest/components/contestHeaderCell/ContestHeaderCell.tsx
@@ -6,15 +6,33 @@ export type ContestHeaderCellProps = {
     readonly textColor: string;
     readonly displayName: string;
     readonly linkHref: string;
+    readonly onStatsButtonClick: () => void;
+    readonly statsIconHref: string;
 };
 
-export const ContestHeaderCell = ({ textColor, displayName, linkHref }: ContestHeaderCellProps): ReactNode => {
+export const ContestHeaderCell = ({
+    textColor,
+    displayName,
+    linkHref,
+    onStatsButtonClick,
+    statsIconHref,
+}: ContestHeaderCellProps): ReactNode => {
     return (
         <td className={clsx(cellClassNames, "gap-x-1")}>
             <span className={textColor}>◉</span>
             <a href={linkHref} target="_blank" rel="noreferrer" className="link link-hover link-primary truncate">
                 {displayName}
             </a>
+            <button onClick={onStatsButtonClick} className="hidden group-hover:block group-focus-within:block">
+                <svg
+                    role="img"
+                    aria-label="Show graph"
+                    className="h-6 w-6 fill-current cursor-pointer hover:invert-30 transition-all"
+                >
+                    <title>Show stats</title>
+                    <use href={statsIconHref} />
+                </svg>
+            </button>
         </td>
     );
 };
