8709cea815db3d8a38669afc384783f4eec66c8a
matcher9131 <matcher9131cd@gmail.com>
2025-08-28 11:29:48 +0900

    Fix bugs: Mistakenly takes rawRatingAtom for ratingAtom in useUserLabel

--- src/features/user/components/userLabel/useUserLabel.tsx
+++ src/features/user/components/userLabel/useUserLabel.tsx
@@ -1,12 +1,13 @@
 import { useAtomValue } from "jotai";
 import type { UserLabelProps } from "./UserLabel";
 import { numContestsAtom, ratingAtom } from "../../../rating/models/atom";
+import { rawRatingAtom } from "../../../rating/models/getter";
 import { RatingWithIconContainer } from "../../../rating/components/ratingWithIcon";
 
 export const useUserLabel = (): UserLabelProps => {
     const rating = useAtomValue(ratingAtom);
     const numContests = useAtomValue(numContestsAtom);
-    const rawRating = useAtomValue(ratingAtom);
+    const rawRating = useAtomValue(rawRatingAtom);
     const rawRatingWithIcon = rawRating != null ? <RatingWithIconContainer rating={rawRating} /> : "-";
     return {
         rating: rating != null ? rating.toString() : null,
