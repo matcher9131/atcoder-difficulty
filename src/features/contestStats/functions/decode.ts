6d05833d32fd8aa7c6c566886d4ccd665a1b9e3b
matcher9131 <matcher9131cd@gmail.com>
2025-09-19 12:33:41 +0900

    Fix contestStats decode function (Trim extra zeros)

--- src/features/contestStats/functions/decode.ts
+++ src/features/contestStats/functions/decode.ts
@@ -1,17 +1,21 @@
 export const decodeContestDistribution = (encodedZeroIndices: string, encodedNonZeroBytes: string): number[] => {
     const zeroIndicesBitArray = Uint8Array.from(atob(encodedZeroIndices), (ch) => ch.charCodeAt(0));
     const nonZeroBytes = Uint8Array.from(atob(encodedNonZeroBytes), (ch) => ch.charCodeAt(0));
-    const res = new Uint8Array(encodedZeroIndices.length * 6);
+    const bytes = new Uint8Array(encodedZeroIndices.length * 6);
 
     let nonZeroIndex = 0;
-    for (let i = 0; i < res.length; ++i) {
+    for (let i = 0; i < bytes.length; ++i) {
         if (zeroIndicesBitArray[i >> 3] & (1 << (7 - (i % 8)))) {
-            res[i] = 0;
+            bytes[i] = 0;
         } else {
-            res[i] = nonZeroBytes[nonZeroIndex];
+            bytes[i] = nonZeroBytes[nonZeroIndex];
             ++nonZeroIndex;
         }
     }
 
-    return Array.from(new Uint16Array(res.buffer));
+    const result = Array.from(new Uint16Array(bytes.buffer));
+    return result.slice(
+        0,
+        result.findLastIndex((val) => val > 0),
+    );
 };
